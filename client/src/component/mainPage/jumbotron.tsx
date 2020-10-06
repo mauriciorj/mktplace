61facf9b113671bb1b7890cefa94e6114195aaf1
mauricio.o.mesquita@gmail.com <mauricio.o.mesquita@gmail.com>
2020-10-06 00:20:06 -0400

    Prismic first configuration + Jumbotron component imported

--- client/src/component/mainPage/jumbotron.tsx
+++ client/src/component/mainPage/jumbotron.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom'
 
@@ -7,28 +7,53 @@ import jumbotronImg from '../../assets/img/mainPage/jumbotron/jumbotronImg.jpg'
 
 import { Row, Col, Button, Typography, Image } from 'antd'
 
-const jumbotron = () => {
+import { Client } from '../../prismic/prismic-configuration'
+
+interface DocPrismic {
+    doc: any
+}
+
+const Jumbotron = () => {
+
+    const [doc, setDocData] = useState<DocPrismic | null>({ doc: null })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Client.getSingle('mainpage-jumbotron', {})
+                if (response) {
+                    setDocData({ doc: response })
+                }
+                console.log(response)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
+
+    console.log(doc)
 
     return (
         <>
-        <Row justify="start">
-            <Col span={9} offset={2} className="jumbotron-main-page-left-side">
-                <Typography.Title level={2} className="jumbotron-main-page-left-title">Your support any time, any where.</Typography.Title>
-                <br />
-                <Typography.Title level={4} className="jumbotron-main-page-left-sub-title">We are here to provide the best support for everyone, any time and everywhere.</Typography.Title>
-                <br />
-                <Link to="/registerOptions"><Button type="primary" shape="round">Create Account</Button></Link>
-            </Col>
-            <Col span={10} offset={1}>
-                <Image src={jumbotronImg} />
-            </Col>
-        </Row>
-        <Row>
-            <Col span={20} offset={2} className="jumbotron-main-page-bottom-border"></Col>
-        </Row>
+            <Row justify="start">
+                <Col span={9} offset={2} className="jumbotron-main-page-left-side">
+                    <Typography.Title level={2} className="jumbotron-main-page-left-title">Your support any time, any where.</Typography.Title>
+                    <br />
+                    <Typography.Title level={4} className="jumbotron-main-page-left-sub-title">We are here to provide the best support for everyone, any time and everywhere.</Typography.Title>
+                    <br />
+                    <Link to="/registerOptions"><Button type="primary" shape="round">Create Account</Button></Link>
+                </Col>
+                <Col span={10} offset={1}>
+                    <Image src={jumbotronImg} />
+                </Col>
+            </Row>
+            <Row>
+                <Col span={20} offset={2} className="jumbotron-main-page-bottom-border"></Col>
+            </Row>
         </>
     )
 
 }
 
-export default jumbotron
+export default Jumbotron
