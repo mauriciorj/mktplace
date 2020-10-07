1e5f7b8ed21f32e480e0810ed72d4f5622a99de1
mauricio.o.mesquita@gmail.com <mauricio.o.mesquita@gmail.com>
2020-10-06 23:45:33 -0400

    Typescript problem fixed

--- web_component_library/webpack.config.js
+++ web_component_library/webpack.config.js
@@ -1,9 +1,28 @@
-module: {
+module.exports = {
+  module: {
     rules: [
+      // All files with a ".ts" or ".tsx" extension will be handled by "awesome-typescript-loader".
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
+        test: /\.(ts|js)x?$/,
+        include: __dirname,
         loader: 'babel-loader'
-      }
+      },
+
+      // All output ".js" files will have any sourcemaps re-processed by "source-map-loader".
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
     ]
+  },
+  resolve: {
+    // Add ".ts" and ".tsx" as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
   }
+}
