3ab73f34cfa5561ea367c552ac5baea1803d8b29
mauricio.o.mesquita@gmail.com <mauricio.o.mesquita@gmail.com>
2020-10-08 17:49:26 -0400

    New WCL without CRA - working

--- web_component_library/theme.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // '@primary-color': '#1DA57A'
-  // '@font-family': 'Lato',
-  // '@font-size-base': '14px',
-  '@primary-color': '#00998F',
-  // '@border-width-base': '1px',
-  // '@border-radius-base': '1px',
-  '@btn-font-weight': 400
-}
