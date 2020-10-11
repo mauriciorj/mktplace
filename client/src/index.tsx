d9c32bf265ae8f51d1837cb3f172343911572f2d
mauricio.o.mesquita@gmail.com <mauricio.o.mesquita@gmail.com>
2020-10-11 01:28:05 -0400

    NEW client created withou CRA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,32 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import App from "./App";
 
-import './assets/googleFont/googleFont.css'
-
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'http://localhost:8080/v1/graphql',
-  }),
-  cache: new InMemoryCache()
-});
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
